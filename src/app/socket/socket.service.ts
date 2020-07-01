e7f6d1b5be6775b31bcd9113d402b8b2c875f533
Isaac Bake <isaac@blake.earth>
2020-07-01 17:31:48 -0400

    Double the socket

--- src/app/socket/socket.service.ts
+++ src/app/socket/socket.service.ts
@@ -1,23 +1,26 @@
-import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { Observable, Subject } from 'rxjs';
 
+const tokenRequest: WebSocketSubject<any> = webSocket('wss://websocket.slumberparty.io/create-token');
 const socket: WebSocketSubject<any> = webSocket('wss://websocket.slumberparty.io');
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
-  http: HttpClient;
 
   replySource: Subject<any>;
   public reply: Observable<any>;
 
-  constructor(http: HttpClient) {
-    this.http = http;
-    this.http.get("https://websocket.slumberparty.io/get-token").subscribe((data: any) => console.log(data));
+  constructor() {
+
+    tokenRequest.subscribe(
+      msg => this.setResponse(msg),
+      err => console.log(err),
+      () => console.log('complete')
+    );
 
     socket.subscribe(
       msg => this.setResponse(msg),
