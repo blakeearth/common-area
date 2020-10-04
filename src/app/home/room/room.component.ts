69d8322ddf132451ecafc0e29783f8e3cf6f1f7f
Isaac Bake <isaac@blake.earth>
2020-10-04 18:19:57 -0400

    Add spritesheet with Kontra

--- src/app/home/room/room.component.ts
+++ src/app/home/room/room.component.ts
@@ -1,60 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { init, Sprite, Text, GameLoop } from 'kontra';
+import { init, TileEngine, load, setImagePath, imageAssets, GameLoop } from 'kontra';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
-export class RoomComponent implements OnInit {
 
-  canvas: any;
-  context: any;
+export class RoomComponent implements OnInit {
 
   constructor() { }
 
-  // ...
   ngOnInit(): void {
+
     let { canvas, context } = init();
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
-    let sprite: Sprite = Sprite({
-      x: 100,        // starting x,y position of the sprite
-      y: 80,
-      color: 'red',  // fill color of the sprite rectangle
-      width: 0.1 * canvas.width,     // width and height of the sprite rectangle
-      height: 0.1 * canvas.width,
-      dx: 2          // move the sprite 2px to the right every frame
-    });
+    context.scale(4, 4);
 
-    let text = Text({
-      text: '🕯️',
-      font: '128px Arial',
-      color: 'white',
-      x: canvas.width / 2,
-      y: canvas.height / 2,
-      dx: 3,
-      anchor: {x: 0.5, y: 0.5},
-      textAlign: 'center'
-    });
+    context.imageSmoothingEnabled = false;
 
-    let loop: GameLoop = GameLoop({  // create the main game loop
-      update: function() { // update the game state
-        sprite.update();
-
-        // wrap the sprites position when it reaches
-        // the edge of the screen
-        if (sprite.x > canvas.width) {
-          sprite.x = -sprite.width;
+    setImagePath('/assets/room/');
+    load(
+      'floor.png'
+    ).then(() => {
+      let img: HTMLImageElement = imageAssets['floor'];
+      console.log(imageAssets);
+      console.log(img);
+      let tileEngine: TileEngine = TileEngine({
+        // tile size
+        tilewidth: 32,
+        tileheight: 32,
+    
+        // map size in tiles
+        width: 4,
+        height: 3,
+    
+        // tileset object
+        tilesets: [{
+          firstgid: 1,
+          image: img
+        }],
+    
+        // layer object
+        layers: [{
+          name: 'ground',
+          data: [ 6,  6,  6,  6,  6,  6,  6,  6,  6,
+                  6,  6,  6,  7,  7,  8,  6,  6,  6,
+                  6,  6,  27, 24, 24, 25, 6,  6,  6,
+                  6,  23, 24, 24, 24, 26, 8,  6,  6,
+                  6,  23, 24, 24, 24, 24, 26, 8,  6,
+                  6,  23, 24, 24, 24, 24, 24, 25, 6,
+                  6,  46, 41, 41, 16, 24, 24, 25, 6,
+                  6,  6,  6,  6,  46, 41, 41, 42, 6,
+                  6,  6,  6,  6,  6,  6,  6,  6,  6 ]
+        }]
+      });
+    
+      let loop: GameLoop = GameLoop({
+        render() {
+          tileEngine.render();
         }
-      },
-      render: function() { // render the game state
-        sprite.render();
-        text.render();
-      }
-    });
-
-    loop.start();    // start the game
+      });
+    
+      // start the loop
+      loop.start();
+      })
+    }
   }
-}
