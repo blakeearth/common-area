3bc6135b9cdb0832f0c9a46c76536926281e4ec1
Isaac Blake <blakeearth@users.noreply.github.com>
2021-11-09 16:03:53 -0500

    Update Angular to 12

--- docs/main-es5.js
+++ docs/main-es5.js
@@ -9067,7 +9067,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
      * This import should be commented out in production mode because it will have a negative impact
      * on performance if an error is thrown.
      */
-    // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+    // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 
     /***/
   },
