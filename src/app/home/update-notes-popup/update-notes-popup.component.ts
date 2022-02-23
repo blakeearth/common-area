2f51f78b9f088819047b5e593307ef73a0dabfda
Isaac Blake <blakeearth@users.noreply.github.com>
2022-02-23 14:57:27 -0500

    Finish and build update notes

--- src/app/home/update-notes-popup/update-notes-popup.component.ts
+++ src/app/home/update-notes-popup/update-notes-popup.component.ts
@@ -12,6 +12,10 @@ export class UpdateNotesPopupComponent implements OnInit {
   data: string[];
   onClose: Function;
 
+  leftVisibility = "hidden";
+  rightVisibility = "hidden";
+  updateNotesIndex = 0;
+
   constructor(sanitizer: DomSanitizer) {
     this.sanitizer = sanitizer;
   }
@@ -20,5 +24,13 @@ export class UpdateNotesPopupComponent implements OnInit {
     let modalContent: HTMLElement = document.getElementsByClassName("modal-content")[0] as HTMLElement;
 
     modalContent.focus();
+
+    this.addIndex(0);
+  }
+
+  addIndex(index: number): void {
+    this.updateNotesIndex += index;
+    this.leftVisibility = this.updateNotesIndex > 0 ? "inherit" : "hidden";
+    this.rightVisibility = this.updateNotesIndex < this.data.length - 1 ? "inherit" : "hidden";
   }
 }
